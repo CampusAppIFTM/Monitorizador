0dab75d141aace12c05d00495005f300e08f3069
aluno <muryel.lirio@estudante.iftm.edu.br>
2026-08-19 10:33:29 -0300

    final

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StyleSheet, Text, View, Button, ActivityIndicator, Image } from "react-
 import { useState } from "react";
 import { GoogleSignin } from "@react-native-google-signin/google-signin";
 
-//funções de autenticação
+
 export const onLogin = async () => {
   const user = await GoogleSignin.signIn();
   return user;
